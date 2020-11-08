45c18092bfc3aa2bff8941f57ad6241c14f470d8
Nasirul Haque Ridoy <dpridoy@gmail.com>
2020-11-08 20:37:06 +0600

    fourth commit order history print

--- order/urls.py
+++ order/urls.py
@@ -5,6 +5,6 @@ app_name='order'
 
 urlpatterns = [
     path('thanks/<int:order_id>/', views.thanks, name='thanks'),
-    # path('history/', views.orderHistory, name='order_history'),
-    # path('<int:order_id>/', views.viewOrder, name='order_detail'),
+    path('history/', views.orderHistory, name='order_history'),
+    path('<int:order_id>/', views.viewOrder, name='order_detail'),
 ]
--- order/views.py
+++ order/views.py
@@ -7,3 +7,19 @@ def thanks(request, order_id):
     if order_id:
         customer_order = get_object_or_404(Order, id=order_id)
     return render(request, 'thanks.html',{'customer_order': customer_order})
+
+@login_required()
+def orderHistory(request):
+    if request.user.is_authenticated:
+        email = str(request.user.email)
+        print(email)
+        order_details = Order.objects.filter(emailAddress=email)
+    return render(request, 'order/orders_list.html', {'order_details':order_details})
+
+@login_required()
+def viewOrder(request, order_id):
+    if request.user.is_authenticated:
+        email = str(request.user.email)
+        order = Order.objects.get(id=order_id, emailAddress=email)
+        order_items = OrderItem.objects.filter(order=order)
+    return render(request, 'order/order_detail.html', {'order':order, 'order_items':order_items})
